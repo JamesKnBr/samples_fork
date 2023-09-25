38f34c85f0f61b094d082725cd87552da4806329
JamesKnBr <james.broady@shell.com>
2023-09-25 16:39:32 +0100

    remove print statement
    Signed-off-by: JamesKnBr <james.broady@shell.com>

--- Python-DeltaSharing-to-Delta/pipeline.py
+++ Python-DeltaSharing-to-Delta/pipeline.py
@@ -2,5 +2,5 @@ from rtdip_sdk.pipelines.sources.python.delta_sharing import PythonDeltaSharingS
 from rtdip_sdk.pipelines.destinations.python.delta import PythonDeltaDestination
 
 source = PythonDeltaSharingSource("{/path/to/config/credential/file}","{name_of_share}", "{schema_name}", "{table_name}").read_batch()
-print(source)
+
 destination = PythonDeltaDestination(source, "{/path/to/table}", mode="append").write_batch()
